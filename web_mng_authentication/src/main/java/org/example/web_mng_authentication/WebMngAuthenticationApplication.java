7111295915cea3ac4924596f1f9eb559e77c4f49
gyeongeun <gyeoungeun0322@gmail.com>
2024-11-18 16:09:39 +0900

    new spring security service
    for user authentication, authorization service

---
+++ web_mng_authentication/src/main/java/org/example/web_mng_authentication/WebMngAuthenticationApplication.java
@@ -0,0 +1,15 @@
+package org.example.web_mng_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient  // eureka client service
+@SpringBootApplication
+public class WebMngAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMngAuthenticationApplication.class, args);
+    }
+
+}
---
+++ web_mng_authentication/src/test/java/org/example/web_mng_authentication/WebMngAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.web_mng_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMngAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
